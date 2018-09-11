f12fd59901197cc0dfa51237895c5dd8e6c4e427
Hyoung Il Jin <gjfaos1245@naver.com>
2018-09-11 19:50:54 +0900

    9월11일 PP02.BreakUpHelloSDL

--- PP02.BreakUpHelloSDL/main.cpp
+++ PP02.BreakUpHelloSDL/main.cpp
@@ -3,6 +3,26 @@
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
 bool g_bRunning = false;
+void render();
+bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
+
+int main(int argc, char* argv[])
+{
+	if (init("PP02.BreakUpHelloSDL Retry", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		640, 480, SDL_WINDOW_SHOWN)) {
+		g_bRunning = true;
+	}
+	else {
+		return 1;
+	}
+
+	while (g_bRunning) {
+		render();
+	}
+	SDL_Quit();
+	return 0;
+}
+
 
 bool init(const char* title, int xpos, int ypos, int width, int height, int flags)
 {
@@ -26,22 +46,7 @@ void render()
 	SDL_SetRenderDrawColor(g_pRenderer, 255, 0, 0, 255);
 	SDL_RenderClear(g_pRenderer);
 	SDL_RenderPresent(g_pRenderer);
-	
-}
-
-int main(int argc, char* argv[])
-{
-	if (init("PP02.BreakUpHelloSDL Retry", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		640, 480, SDL_WINDOW_SHOWN)) {
-		g_bRunning = true;
-	}
-	else {
-		return 1;
-	}
-
-	while (g_bRunning) {
-		render();
-	}
+	SDL_Delay(5000);
 	SDL_Quit();
-	return 0;
 }
+
