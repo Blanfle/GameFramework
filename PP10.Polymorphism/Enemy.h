c692b05bc42902fcd2c6f6cdc00ab595a765ce7a
user <user@532-12>
2018-10-18 18:21:09 +0900

    PP10.Polymorphism 10/18

---
+++ PP10.Polymorphism/Enemy.h
@@ -0,0 +1,11 @@
+#include "Player.h"
+
+class Enemy : public GameObject
+{
+public:
+	void load(int x, int y, int width, int height,
+		std::string textureID);
+	void draw(SDL_Renderer* pRenderer);
+	void update();
+	void clean();
+};
---
+++ PP10.Polymorphism/Game.h
@@ -0,0 +1,32 @@
+#include "TextureManager.h"
+#include "Enemy.h"
+#include <vector>
+
+class Game
+{
+	std::vector<GameObject*> m_gameObjects;
+
+public:
+	Game() {}
+	~Game() {}
+	bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool running() { return m_bRunning; }
+	int m_currentFrame;
+	TextureManager m_textureManager;
+	GameObject* m_go;
+	GameObject* m_player;
+	GameObject* m_enemy;
+
+private:
+	SDL_Texture * m_pTexture;
+	SDL_Rect m_sourceRectangle;
+	SDL_Rect m_destinationRectangle;
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+protected:
+};
