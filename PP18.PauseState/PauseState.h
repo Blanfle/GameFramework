ce7f79927830606532cda0b1218ac4357ca845a7
Hyoung Il Jin <gjfaos1245@naver.com>
2018-12-17 19:45:52 +0900

    PP18.PauseState

---
+++ PP18.PauseState/PauseState.h
@@ -0,0 +1,30 @@
+#pragma once;
+#include "GameState.h"
+#include "Game.h"
+#include "MenuButton.h"
+
+
+class GameObject;
+
+class PauseState : public GameState
+{
+public:
+	static PauseState* Instance()
+	{
+		if (s_pInstance == 0) {
+			s_pInstance = new PauseState();
+		}
+		return s_pInstance;
+	}
+	virtual void update();
+	virtual void render();
+	virtual bool onEnter();
+	virtual bool onExit();
+	virtual std::string getStateID() const { return s_pauseID; }
+private:
+	static void s_pauseToMain();
+	static void s_resumePlay();
+	static const std::string s_pauseID;
+	static PauseState* s_pInstance;
+	std::vector<GameObject*> m_gameObjects;
+};
