3942be77ead70b5532b91a3419ecbd782bb1c9b9
Hyoung Il Jin <gjfaos1245@naver.com>
2018-12-17 19:19:01 +0900

    PP16.MenuState

---
+++ PP16.MenuState/Player.cpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "Player.h"
+
+Player::Player(const LoaderParams* pParams) : SDLGameObject(pParams)
+{
+}
+void Player::draw()
+{
+}
+void Player::update()
+{
+}
+void Player::clean()
+{
+}
+
+void Player::handleInput()
+{
+}
